f73ce21916646076d351cd82d250e05c50b3e7d2
flyninja <flyninja@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2015-06-27 22:01:02 +0000

    #1031 - debug-log в ie8--
    git-svn-id: svn://alcomputer/RunaWFE-4.x/branches/wfe.1031@6402 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe-web/src/main/resources/scripts/ViewUtil.UserTypeListTable.js
+++ wfe-web/src/main/resources/scripts/ViewUtil.UserTypeListTable.js
@@ -8,14 +8,19 @@ if (!window.console) {
 if (!window.console.log) {
 	window.console.log = function(msg) {};
 }
+
+var _internalLog = function () {
+	console.log.apply(this, arguments);
+}
+
 if (!window.console.info) {
-	window.console.info = function(msg) {};
+	window.console.info = _internalLog;
 }
 if (!window.console.debug) {
-	window.console.debug = function(msg) {};
+	window.console.debug = _internalLog;
 }
 if (!window.console.error) {
-	window.console.error = function(msg) {};
+	window.console.error = _internalLog;
 }
 
 $(document).ready(function() {
