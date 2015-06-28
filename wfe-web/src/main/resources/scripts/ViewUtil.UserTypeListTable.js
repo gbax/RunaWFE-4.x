1f47ba3ea8656eeacceb6015db8cf0265722718c
flyninja <flyninja@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2015-06-28 11:41:33 +0000

    #1031 - вынос safelog в отдельный скрипт
    git-svn-id: svn://alcomputer/RunaWFE-4.x/branches/wfe.1031@6403 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe-web/src/main/resources/scripts/ViewUtil.UserTypeListTable.js
+++ wfe-web/src/main/resources/scripts/ViewUtil.UserTypeListTable.js
@@ -1,28 +1,6 @@
 
 var jsonInputArrayUNIQUENAME = JSONDATATEMPLATE;
 
-if (!window.console) {
-	window.console = {};
-}
-
-if (!window.console.log) {
-	window.console.log = function(msg) {};
-}
-
-var _internalLog = function () {
-	console.log.apply(this, arguments);
-}
-
-if (!window.console.info) {
-	window.console.info = _internalLog;
-}
-if (!window.console.debug) {
-	window.console.debug = _internalLog;
-}
-if (!window.console.error) {
-	window.console.error = _internalLog;
-}
-
 $(document).ready(function() {
 	
 	try {
