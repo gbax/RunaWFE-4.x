87e8e0ca0dd38cdfe4592df9c123fb370482917f
flyninja <flyninja@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2015-06-29 11:49:52 +0000

    #1031 - мерж wfe
    git-svn-id: svn://alcomputer/RunaWFE-4.x/trunk@6409 999c22fd-c5eb-e245-b5be-d41808c6d2cd
    
    Former-commit-id: 5ac9ffa742c87890b5b930a145931e81b820828c

--- projects/wfe/wfe-web/src/main/java/ru/runa/wf/web/ftl/method/MultipleSelectFromListTag.java
+++ projects/wfe/wfe-web/src/main/java/ru/runa/wf/web/ftl/method/MultipleSelectFromListTag.java
@@ -5,7 +5,12 @@ import java.util.List;
 import ru.runa.wfe.commons.ftl.FreemarkerTag;
 import ru.runa.wfe.commons.ftl.FtlTagVariableSubmissionPostProcessor;
 import ru.runa.wfe.user.Executor;
+import ru.runa.wfe.var.ComplexVariable;
 import ru.runa.wfe.var.ISelectable;
+import ru.runa.wfe.var.dto.WfVariable;
+import ru.runa.wfe.var.file.IFileVariable;
+import ru.runa.wfe.var.format.FileFormat;
+import ru.runa.wfe.var.format.UserTypeFormat;
 
 import com.google.common.collect.Lists;
 
@@ -37,6 +42,24 @@ public class MultipleSelectFromListTag extends FreemarkerTag implements FtlTagVa
                 Executor executor = (Executor) option;
                 optionValue = "ID" + executor.getId();
                 optionLabel = executor.getLabel();
+            } else if (option instanceof ComplexVariable) {
+                ComplexVariable cvar = (ComplexVariable) option;
+                UserTypeFormat formatter = new UserTypeFormat(cvar.getUserType());
+                optionValue = formatter.formatJSON(cvar);
+                optionValue = optionValue.replaceAll("\"", "&quot;");
+                WfVariable variable = ViewUtil.createVariable(variableName, cvar.getUserType().getName(), formatter, cvar);
+                String hid = cvar.getUserType().getAttributes().get(0) != null ? cvar.getUserType().getAttributes().get(0).getName() + " "
+                        + cvar.get(cvar.getUserType().getAttributes().get(0).getName()) : cvar.getUserType().getName();
+                optionLabel = variableName + " " + hid + "<br>"
+                        + ViewUtil.getComponentOutput(user, webHelper, variableProvider.getProcessId(), variable);
+            } else if (option instanceof IFileVariable) {
+                FileFormat formatter = new FileFormat();
+                IFileVariable file = (IFileVariable) option;
+                optionValue = formatter.formatJSON(file);
+                optionValue = optionValue.replaceAll("\"", "&quot;");
+                WfVariable variable = ViewUtil.createVariable(file.getName(), variableName, formatter, file);
+                optionLabel = variableName + ": " + file.getName() + "<br>"
+                        + ViewUtil.getComponentOutput(user, webHelper, variableProvider.getProcessId(), variable);
             } else {
                 optionValue = String.valueOf(option);
                 optionLabel = String.valueOf(option);
