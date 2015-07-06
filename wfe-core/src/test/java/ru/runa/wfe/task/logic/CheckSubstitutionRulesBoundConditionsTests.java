9e6ea9ba9a7b951bacf9e0b46ea53a287d5fe740
flyninja <flyninja@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2015-07-06 22:09:39 +0000

    #187 - мок-фабрика - контейнер тест-кейсов - граничное условие тестирования правил замещения - вложенные документированные исключения
    git-svn-id: svn://alcomputer/RunaWFE-4.x/branches/wfe.187@6460 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe-core/src/test/java/ru/runa/wfe/task/logic/CheckSubstitutionRulesBoundConditionsTests.java
+++ wfe-core/src/test/java/ru/runa/wfe/task/logic/CheckSubstitutionRulesBoundConditionsTests.java
@@ -1,20 +1,93 @@
 package ru.runa.wfe.task.logic;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Set;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import ru.runa.wfe.execution.ExecutionContext;
+import ru.runa.wfe.ss.SubstitutionCriteria;
+import ru.runa.wfe.task.Task;
+import ru.runa.wfe.user.Actor;
+import ru.runa.wfe.user.ExecutorDoesNotExistException;
+import ru.runa.wfe.user.dao.IExecutorDAO;
+
+import com.google.common.collect.Sets;
 
 @Test
 @ContextConfiguration(locations = { "classpath:ru/runa/wfe/task/logic/test.context.xml" })
 public class CheckSubstitutionRulesBoundConditionsTests extends AbstractTestNGSpringContextTests {
 
+    private static final Log log = LogFactory.getLog(CheckSubstitutionRulesBoundConditionsTests.class);
+
     @Autowired
     ITaskListBuilderTestProvider taskListBuilder;
 
-    @Test()
-    void test1() {
+    @DataProvider
+    public Object[][] getExceptionsTestcases() {
+        return new Object[][] { { "applies with one of DataAccessException testcase", new TestCaseDataSet() {
+            @Override
+            public void mockRules(IExecutorDAO executorDAO) {
+                when(executorDAO.getActor(new Long(1))).thenReturn(mock(Actor.class));
+                when(executorDAO.getActor(new Long(2))).thenThrow(mock(org.springframework.dao.DataAccessException.class));
+                when(executorDAO.getActor(new Long(3))).thenReturn(mock(Actor.class));
+                when(executorDAO.getActor(new Long(4))).thenReturn(mock(Actor.class));
+            }
 
+            @Override
+            public SubstitutionCriteria getCriteria() {
+                SubstitutionCriteria criteria = mock(SubstitutionCriteria.class);
+                when(criteria.isSatisfied(any(ExecutionContext.class), any(Task.class), any(Actor.class), any(Actor.class))).thenReturn(true);
+                return criteria;
+            }
+
+            @Override
+            public Set<Long> getIds() {
+                return Sets.newHashSet(new Long(1), new Long(2), new Long(3), new Long(4));
+            }
+
+        } }, { "applies with one of ExecutorDoesNotExistException testcase", new TestCaseDataSet() {
+            @Override
+            public void mockRules(IExecutorDAO executorDAO) {
+                when(executorDAO.getActor(new Long(1))).thenReturn(mock(Actor.class));
+                when(executorDAO.getActor(new Long(2))).thenThrow(mock(ExecutorDoesNotExistException.class));
+                when(executorDAO.getActor(new Long(3))).thenReturn(mock(Actor.class));
+                when(executorDAO.getActor(new Long(4))).thenReturn(mock(Actor.class));
+            }
+
+            @Override
+            public SubstitutionCriteria getCriteria() {
+                SubstitutionCriteria criteria = mock(SubstitutionCriteria.class);
+                when(criteria.isSatisfied(any(ExecutionContext.class), any(Task.class), any(Actor.class), any(Actor.class))).thenReturn(true);
+                return criteria;
+            }
+
+            @Override
+            public Set<Long> getIds() {
+                return Sets.newHashSet(new Long(1), new Long(2), new Long(3), new Long(4));
+            }
+
+        } } };
     }
 
+    @Test(dataProvider = "getExceptionsTestcases")
+    void exceptionsTests(String testName, TestCaseDataSet testCase) {
+
+        log.info(String.format("start test: %s", testName));
+
+        int rules = taskListBuilder.checkSubstitutionRules(testCase.getCriteria(), testCase.getIds(), testCase.getExeContext(), testCase.getTask(),
+                testCase.getAssignedActor(), testCase.getSubstitutorActor());
+
+        Assert.assertEquals(rules, TaskListBuilder.SUBSTITUTION_APPLIES);
+    }
 }
--- wfe-core/src/test/java/ru/runa/wfe/task/logic/TaskLogicMockFactory.java
+++ wfe-core/src/test/java/ru/runa/wfe/task/logic/TaskLogicMockFactory.java
@@ -1,5 +1,7 @@
 package ru.runa.wfe.task.logic;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
 import ru.runa.wfe.audit.ProcessLog;
 import ru.runa.wfe.audit.dao.IProcessLogDAO;
 import ru.runa.wfe.commons.dao.IGenericDAO;
@@ -13,43 +15,77 @@ import ru.runa.wfe.user.dao.IExecutorDAO;
 
 public class TaskLogicMockFactory {
 
+    private static TaskLogicMockFactory singleton;
+
+    private final IWfTaskFactory taskFactory = mock(IWfTaskFactory.class);
+    private final IExecutorDAO executorDAO = mock(IExecutorDAO.class);
+    private final ISubstitutionLogic substitutionLogic = mock(ISubstitutionLogic.class);
+    private final IProcessDefinitionLoader processDefinitionLoader = mock(IProcessDefinitionLoader.class);
+    @SuppressWarnings("unchecked")
+    private final IGenericDAO<Task> taskDAO = mock(IGenericDAO.class);
+    private final IExecutorContextFactory exeContextFactory = mock(IExecutorContextFactory.class);
+    private final IBatchPresentationCompilerFactory<?> batchCompilerFactory = mock(IBatchPresentationCompilerFactory.class);
+    @SuppressWarnings("unchecked")
+    private final IProcessLogDAO<ProcessLog> logDAO = mock(IProcessLogDAO.class);
+
+    public static final TaskLogicMockFactory getFactory() {
+        return singleton;
+    }
+
+    public TaskLogicMockFactory() {
+        singleton = this;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void setContextRules(ITestCaseDataSet dataset) {
+        reset(taskFactory);
+        reset(executorDAO);
+        reset(substitutionLogic);
+        reset(processDefinitionLoader);
+        reset(taskDAO);
+        reset(exeContextFactory);
+        reset(batchCompilerFactory);
+        reset(logDAO);
+
+        dataset.mockRules(batchCompilerFactory);
+        dataset.mockRules(exeContextFactory);
+        dataset.mockRules(executorDAO);
+        dataset.mockRules(taskDAO);
+        dataset.mockRules(processDefinitionLoader);
+        dataset.mockRules(logDAO);
+        dataset.mockRules(substitutionLogic);
+        dataset.mockRules(taskFactory);
+    }
+
     public IWfTaskFactory createMockWfTaskFactory() {
-        /* TODO: implement */
-        return null;
+        return taskFactory;
     }
 
     public IExecutorDAO createMockExecutorDAO() {
-        /* TODO: implement */
-        return null;
+        return executorDAO;
     }
 
     public ISubstitutionLogic createMockSubstitutionLogic() {
-        /* TODO: implement */
-        return null;
+        return substitutionLogic;
     }
 
     public IProcessDefinitionLoader createMockProcessDefinitionLoader() {
-        /* TODO: implement */
-        return null;
+        return processDefinitionLoader;
     }
 
     public IGenericDAO<Task> createMockGenericDAO() {
-        /* TODO: implement */
-        return null;
+        return taskDAO;
     }
 
     public IExecutorContextFactory createMockExecutorContextFactory() {
-        /* TODO: implement */
-        return null;
+        return exeContextFactory;
     }
 
     public IBatchPresentationCompilerFactory<?> createMockBatchPresentationCompilerFactory() {
-        /* TODO: implement */
-        return null;
+        return batchCompilerFactory;
     }
 
     public IProcessLogDAO<ProcessLog> createMockProcessLogDAO() {
-        /* TODO: implement */
-        return null;
+        return logDAO;
     }
 }
