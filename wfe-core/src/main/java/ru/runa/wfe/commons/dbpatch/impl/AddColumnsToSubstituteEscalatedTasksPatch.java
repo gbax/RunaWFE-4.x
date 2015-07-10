efe4f36bd079887e26e5510894b4d7e708f062a8
flyninja <flyninja@999c22fd-c5eb-e245-b5be-d41808c6d2cd>
2015-07-10 21:29:43 +0000

    #1073 - получение PROCESS_ID из DESCRIPTION там где это возможно
    git-svn-id: svn://alcomputer/RunaWFE-4.x/branches/wfe.187@6472 999c22fd-c5eb-e245-b5be-d41808c6d2cd

--- wfe-core/src/main/java/ru/runa/wfe/commons/dbpatch/impl/AddColumnsToSubstituteEscalatedTasksPatch.java
+++ wfe-core/src/main/java/ru/runa/wfe/commons/dbpatch/impl/AddColumnsToSubstituteEscalatedTasksPatch.java
@@ -2,19 +2,26 @@ package ru.runa.wfe.commons.dbpatch.impl;
 
 import java.sql.Types;
 import java.util.List;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import ru.runa.wfe.commons.dbpatch.DBPatch;
+import ru.runa.wfe.execution.dao.ProcessDAO;
 
 public class AddColumnsToSubstituteEscalatedTasksPatch extends DBPatch {
+
+    private static final Pattern DECIMAL_LONG = Pattern.compile("([\\d]+)");
+
+    @Autowired
+    private ProcessDAO processDAO;
 
     @Override
     protected List<String> getDDLQueriesBefore() {
         List<String> sql = super.getDDLQueriesAfter();
-
-        // sql.add(getDDLRemoveColumn("EXECUTOR", "PROCESS_ID"));
-        // sql.add(getDDLRemoveColumn("EXECUTOR", "NODE_ID"));
 
         sql.add(getDDLCreateColumn("EXECUTOR", new ColumnDef("PROCESS_ID", Types.BIGINT, true)));
         sql.add(getDDLCreateColumn("EXECUTOR", new ColumnDef("NODE_ID", dialect.getTypeName(Types.VARCHAR, 255, 255, 255), true)));
@@ -27,18 +34,35 @@ public class AddColumnsToSubstituteEscalatedTasksPatch extends DBPatch {
     @SuppressWarnings("unchecked")
     @Override
     protected void applyPatch(Session session) throws Exception {
-        List<Number> ids = session.createSQLQuery("SELECT ID FROM EXECUTOR WHERE DISCRIMINATOR IN ('E', 'T')").list();
+        List<Object[]> bundles = session.createSQLQuery("SELECT ID, DESCRIPTION FROM EXECUTOR WHERE DISCRIMINATOR IN ('E', 'T')").list();
 
-        for (Number id : ids) {
+        for (Object[] bundle : bundles) {
             try {
-                log.info(String.format("applyPatch: id: %s set PROCESS_ID", id));
-                session.createSQLQuery(String.format("UPDATE EXECUTOR SET PROCESS_ID=0 WHERE ID=%s", id)).executeUpdate();
+                log.info(String.format("applyPatch: id: %s set PROCESS_ID description: %s", bundle[0], bundle[1]));
+                Long pid = new Long(0);
+                Matcher m;
+                if (bundle[1] != null && ((m = DECIMAL_LONG.matcher((CharSequence) bundle[1])).find())) {
+                    MatchResult mr = m.toMatchResult();
+                    if (mr.groupCount() >= 1) {
+                        String possiblePid = mr.group(1);
+                        try {
+                            Long temp = Long.parseLong(possiblePid);
+                            if (processDAO.get(temp) != null) {
+                                log.info(String.format("applyPatch: in id: %s found pid: %s by description", bundle[0], temp));
+                                pid = temp;
+                            }
+                        } catch (Exception e) {
+                            /* skip if process with pid is not found */
+                        }
+                    }
+                }
+                session.createSQLQuery(String.format("UPDATE EXECUTOR SET PROCESS_ID=%s WHERE ID=%s", pid, bundle[0])).executeUpdate();
             } catch (Exception e) {
-                log.warn(String.format("applyPatch: set PROCESS_ID for id: %s exc: %s", id, e));
+                log.warn(String.format("applyPatch: set PROCESS_ID for id: %s exc: %s", bundle[0], e));
             }
         }
 
-        ids = session.createSQLQuery("SELECT ID FROM EXECUTOR WHERE DISCRIMINATOR IN ('E')").list();
+        List<Number> ids = session.createSQLQuery("SELECT ID FROM EXECUTOR WHERE DISCRIMINATOR IN ('E')").list();
 
         for (Number id : ids) {
             log.info(String.format("applyPatch: id: %s set NODE_ID", id));
